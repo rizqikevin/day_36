6d95eb393605c0f2e01d8aefe1fc2c85d83274a3
rizqikevino <rizqikevino@gmail.com>
2024-10-31 10:43:51 +0700

    Automation program successfully executed

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
-const dc = require('./dc');
+require('dotenv').config(); 
+const puppeteer = require('puppeteer');
 
-const BASE_URL = 'discord.com';
+const BASE_URL = 'https://discord.com/login';
 
-(async () => {
-    // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+const discord = {
+    browser: null,
+    page: null,
+
+    Initialize: async () => {
+        discord.browser = await puppeteer.launch({ headless: false }); 
+        discord.page = await discord.browser.newPage();
+        await discord.page.goto(BASE_URL, { waitUntil: 'networkidle2' });
+
+        console.log("Navigated to Discord login page");
+    },
+
+    login: async () => {
+        await discord.page.waitForSelector('input[name="email"]');
+        await discord.page.type('input[name="email"]', process.env.DISCORD_EMAIL, { delay: 100 });
+
+      
+        await discord.page.waitForSelector('input[name="password"]');
+        await discord.page.type('input[name="password"]', process.env.DISCORD_PASSWORD, { delay: 100 });
+
   
-    // Navigate the page to a URL
-    await page.goto('BASE_URL');
-})
+        await discord.page.click('button[type="submit"]');
+        console.log("Login button clicked");
+
+        await discord.page.waitForNavigation({ waitUntil: 'networkidle2' });
+        console.log("Login successful!");
+    },
+
+    close: async () => {
+        if (discord.browser) {
+            await discord.browser.close();
+            console.log("Browser closed");
+        }
+    }
+};
+
+// Inisialisasi dan login
+discord.Initialize()
+    .then(() => discord.login())
+    .catch((error) => console.error("Error during automation:", error))
+    .finally(() => discord.close());
